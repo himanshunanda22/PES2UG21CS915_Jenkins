db8f0687d59cb0b6b7ebc3f96b1dfc2f734c6b67
HIMANSHU NANDA <69206689+himanshunanda22@users.noreply.github.com>
2024-03-06 22:18:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Webhook enabled";
+  cout << "Webhook enabled\n";
   return 0;
 }
 // himanshu here making change for the task 4
